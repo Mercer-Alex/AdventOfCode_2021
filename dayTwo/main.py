05cb125a52b536d6080226649c62aee41ee582d8
Alex Mercer <a.mercer118@gmail.com>
2022-01-04 13:27:42 -0700

    part 2 done

--- dayTwo/main.py
+++ dayTwo/main.py
@@ -1,5 +1,6 @@
 subDepth = 0
 subPosition = 0
+aim = 0
 finalPosition = 0
 
 fileInput = open("input.txt", "r")
@@ -7,15 +8,18 @@ for line in fileInput:
     x = line.split(" ")
     if x[0] == "forward":
         subPosition += int(x[1])
+        subDepth += aim * int(x[1])
     elif x[0] == "down":
-        subDepth += int(x[1])
+        aim += int(x[1])
     elif x[0] == "up":
-        subDepth -= int(x[1])
+        aim -= int(x[1])
 
 fileInput.close()
 
 finalPosition = subPosition * subDepth
 print(finalPosition)
+
+
 
 
 
